e582dac65f553b60d5337ef4f261bdd9cf38c96e
Asrar <asrarasrar41891@gmail.com>
2026-07-29 18:44:02 +0530

    Completed Python Day 2 to Day 5 practice

---
+++ src/day2/Calculator.py
@@ -0,0 +1,10 @@
+First_no = int(input("enter first number: "))
+Second_no int(input("Enter Second number: "))
+Addition = First_no + Second_no
+Subtraction = First_no - Second_no
+Multiplication = First_no * Second_no
+Division = First_no / Second_no
+print(Addition,"Addition")
+print(Subtraction, "Subtractuon")
+print(Multipliaction, "Multiplication")
+print(Division, "Division")
---
+++ src/day5/sq.py
@@ -0,0 +1,25 @@
+def square(n):
+    return n * n
+
+def cube(n):
+    retrun n * n * n
+
+def add(a, b):
+    return a + b
+
+def odd(n):
+    if n % 2 != 0:
+        return True
+    return even(n - 1)
+
+
+def max(a,b,c,d):
+    result max(1,2,3,4)
+    print(result)
+
+
+
+def min(a,b,c,d):
+    result min(1,2,3,4)
+    print(result)
+
